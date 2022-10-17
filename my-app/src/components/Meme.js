f3f2ddb5a0265592cfe0ed5a866a92e25bfc1a63
karinastha <karinashrestha19@gmail.com>
2022-10-17 14:30:21 +0545

    "Image processing"

--- my-app/src/components/Meme.js
+++ my-app/src/components/Meme.js
@@ -1,10 +1,29 @@
 import React from "react"
+import memesData from "../memeData"
 import Logo from "../images/logo1.png"
 
 export default function Meme() {
+    // let url
+    // 
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    const [memeImage, setMemeImage] = React.useState("")
     function getMemeImage() {
-        console.log("Clicked")
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        // console.log(url)
     }
+
     return (
         <main>
             <div className="form">
@@ -24,6 +43,8 @@ export default function Meme() {
                     Get a new meme image
                 </button>
             </div>
+            <img src={meme.randomImage} />
         </main>
     )
 }
+// ternary: let check = isFavourite ? "Yes" : "No"
--- my-app/src/memeData.js
+++ my-app/src/memeData.js
@@ -21,7 +21,7 @@ export default {
             {
                 "id": "61579",
                 "name": "One Does Not Simply",
-                "url": "https://i.imgflip.com/1bij.jpg",
+                "url": "https://cdn.pixabay.com/photo/2017/07/25/02/56/sukanah-2536759_960_720.png",
                 "width": 568,
                 "height": 335,
                 "box_count": 2
@@ -29,12 +29,21 @@ export default {
             {
                 "id": "101470",
                 "name": "Ancient Aliens",
-                "url": "https://i.imgflip.com/26am.jpg",
+                "url": "https://cdn.pixabay.com/photo/2019/08/31/11/09/meme-4443243_960_720.jpg",
                 "width": 500,
                 "height": 437,
                 "box_count": 2
             },
-          
+            {
+                "id": "101270",
+                "name": "Ancient Aliens",
+                "url": "https://cdn.pixabay.com/photo/2019/08/31/11/09/meme-4443243_960_720.jpg",
+                "width": 500,
+                "height": 437,
+                "box_count": 2
+            },
+
+
         ]
     }
 }
