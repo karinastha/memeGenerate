b782a73de7d6f4d4c942a5e0196d9e95c3e9986f
karinastha <karinashrestha19@gmail.com>
2022-10-17 13:49:42 +0545

    "Data added"

--- my-app/src/Apprandom.js
+++ my-app/src/Apprandom.js
@@ -1,13 +1,22 @@
 import react from "react";
 
-export default function App(){
-    return(
+export default function App() {
+    return (
         <div className="container">
-            <img src=""/>
+            <img src="" />
 
             <button> Click Me </button>
         </div>
     )
 }
-.addEventListener("click", function() )
-
+.addEventListener("click", function () )
+//useState
+const [count, setCount] = React.useState(0)
+function add() {
+    setCount(count + 1)
+}
+function minus() {
+    setCount(count - 1)
+}
+{/* <button className="counter--plus" onClick={add}> + </button>
+<button className="counter--minus" onClick={minus}> -</button> */}
